a13bbf1fd79852c9faeb8bd4992f37840af03ebe
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2023-03-24 17:58:58 +0900

    blade追加

--- resources/views/livewire/google-search.blade.php
+++ resources/views/livewire/google-search.blade.php
@@ -1,3 +1,16 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h1>検索</h1>
+    <input type="text" wire:model="query" placeholder="文字を入力してください">
+    <button wire:click="search">検索</button>
+
+    @if (!empty($results))
+        <ul>
+            @foreach ($results as $result)
+                <li>
+                    <h2><a href="{{ $result['link'] }}" target="_blank">{{ $result['title'] }}</a></h2>
+                    <p>{{ $result['snippet'] }}</p>
+                </li>
+            @endforeach
+        </ul>
+    @endif
 </div>
---
+++ resources/views/search.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>検索</title>
+    @livewireStyles
+</head>
+
+<body>
+    @livewire('google-search')
+    @livewireScripts
+</body>
+
+</html>
