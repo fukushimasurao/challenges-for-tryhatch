4a558af5c14bef8e3d23717625c6e59d5f26db90
fukushima hayao <nu0640042@gmail.con>
2023-03-27 15:25:27 +0900

    バリデーション、tailwind追加

--- app/Http/Livewire/GoogleSearch.php
+++ app/Http/Livewire/GoogleSearch.php
@@ -10,17 +10,19 @@ class GoogleSearch extends Component
     public $query;
     public $results = [];
 
+    protected $rules = [
+         'query' => 'required|string|min:4|max:255',
+    ];
+
     public function search()
     {
-        if (!empty($this->query)) {
-            $apiKey = config('app.api_key');
-            $searchEngineId = config('app.search_engine_id');
-            $url = "https://www.googleapis.com/customsearch/v1?key={$apiKey}&cx={$searchEngineId}&q={$this->query}";
-            $response = Http::get($url);
-            $this->results = $response->json()['items'];
-        } else {
-            $this->results = [];
-        }
+        $this->validate();
+
+        $apiKey = config('app.api_key');
+        $searchEngineId = config('app.search_engine_id');
+        $url = "https://www.googleapis.com/customsearch/v1?key={$apiKey}&cx={$searchEngineId}&q={$this->query}";
+        $response = Http::get($url);
+        $this->results = $response->json()['items'];
     }
 
     public function render()
--- resources/views/livewire/google-search.blade.php
+++ resources/views/livewire/google-search.blade.php
@@ -1,7 +1,11 @@
 <div>
-    <h1>検索</h1>
+    <h1>Google検索</h1>
     <input type="text" wire:model="query" placeholder="文字を入力してください">
     <button wire:click="search">検索</button>
+
+    @error('query')
+    <span class="error">{{ $message }}</span>
+    @enderror
 
     @if (!empty($results))
         <ul>
@@ -12,5 +16,10 @@
                 </li>
             @endforeach
         </ul>
+    @else
+        <ul>
+            <h2>一致する情報は見つかりませんでした。</h2>
+        </ul>
     @endif
+
 </div>
