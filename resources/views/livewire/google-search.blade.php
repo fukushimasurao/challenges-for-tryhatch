bad255ec14f79df8f8621076d63feb871fac1d45
fukushima hayao <nu0640042@gmail.con>
2023-03-27 16:39:21 +0900

    view修正

--- resources/views/livewire/google-search.blade.php
+++ resources/views/livewire/google-search.blade.php
@@ -1,25 +1,53 @@
 <div>
-    <h1>Google検索</h1>
-    <input type="text" wire:model="query" placeholder="文字を入力してください">
-    <button wire:click="search">検索</button>
+    <div id="services" class="section relative pt-20 pb-8 md:pt-16 md:pb-0 bg-white">
+        <div class="container xl:max-w-6xl mx-auto px-4">
+            <header class="text-center mx-auto mb-12 lg:px-20">
+                <h2 class="text-2xl leading-normal mb-2 font-bold text-black">Google検索</h2>
+            </header>
+            <div class="max-w-2xl mx-auto">
+                <div class="flex justify-center items-center xs:flex-none">
+                    <form class="flex items-center" wire:submit.prevent="submit">
+                        <div class="relative w-full">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                            </div>
+                            <input type="text" wire:model="query"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="文字を入力してください。" required>
+                        </div>
+                        <button type="submit" wire:click="search"
+                            class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                            </svg>
+                        </button>
+                    </form>
+                </div>
 
-    @error('query')
-    <span class="error">{{ $message }}</span>
-    @enderror
 
-    @if (!empty($results))
-        <ul>
-            @foreach ($results as $result)
-                <li>
-                    <h2><a href="{{ $result['link'] }}" target="_blank">{{ $result['title'] }}</a></h2>
-                    <p>{{ $result['snippet'] }}</p>
-                </li>
-            @endforeach
-        </ul>
-    @else
-        <ul>
-            <h2>一致する情報は見つかりませんでした。</h2>
-        </ul>
-    @endif
+                @error('query')
+                    <span class="error">{{ $message }}</span>
+                @enderror
 
+                @if (!empty($results))
+                    <ul>
+                        @foreach ($results as $result)
+                            <li>
+                                <h2><a href="{{ $result['link'] }}" target="_blank">{{ $result['title'] }}</a></h2>
+                                <p>{{ $result['snippet'] }}</p>
+                            </li>
+                        @endforeach
+                    </ul>
+                @endif
+            </div>
+
+        </div>
+    </div>
 </div>
